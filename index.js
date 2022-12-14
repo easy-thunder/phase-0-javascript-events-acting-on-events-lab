b702122f670c693de4c67ab8198251ea95be8e59
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-14 11:14:20 -0700

    commit tree 70ba6932fbe52427910c7e41c7e7f02599d4d8d8

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+let dodger = document.querySelector('#dodger');
+
+dodger.style.left = "180px"
+dodger.style.background = "pink"
+
+
+document.addEventListener('keydown',function(e){
+   if( e.key === "ArrowLeft"){
+
+  moveDodgerLeft();
+    }
+ } )
+
+ document.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+ })
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left>0){
+        dodger.style.left = `${left - 7 }px`
+    }
+}
+
+
+function moveDodgerRight(){
+   const rightNumbers = dodger.style.left.replace('px',"");
+   const right = parseInt(rightNumbers, 10);
+   if(right<360){
+   dodger.style.left = `${right + 7}px`}
+}
+
+
+
+
+
